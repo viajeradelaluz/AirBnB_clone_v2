fa98083c5d0ec6ea9b605f5940fd9b3e5f40936b
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-05-15 18:12:49 -0500

    feat: deletes objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls == None:
+            return FileStorage.__objects
+        obj_by_class = {}
+        for key, value in FileStorage.__objects.items():
+            if value.__class__ == cls:
+                obj_by_class[key] = value
+        return obj_by_class
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +54,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """ Delete an obj from self.__objects """
+        for key, value in self.__objects.items():
+            if value == obj:
+                del self.__objects[key]
+                break
