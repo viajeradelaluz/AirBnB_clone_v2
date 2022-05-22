f020d3458a8f50858d69e5be8158c1bd63ec77f1
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-05-22 18:00:19 -0500

    fix: add exit 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -20,3 +20,4 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '27i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
+exit 0
