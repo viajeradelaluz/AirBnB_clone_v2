f7a0ba9e40d4324b1e1c75b8e366eac1cd428135
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-05-29 19:17:42 -0500

    fix: space deleted on line 20

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -12,10 +12,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_world():
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
     return 'Hello HBNB!'
 
 
-if __name__ == '__main__ ':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
